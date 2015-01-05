52b6ead2dd375d4e81426d9762751fc4fb47fdb3
Scott Rice <scottrice10@gmail.com>
2015-01-05 12:18:53 -0800

    Changed script to use node instead of python.

--- cloudsearchifyDocuments.js
+++ cloudsearchifyDocuments.js
@@ -2,43 +2,52 @@
 // It 1) adds id and type fields to each object, since Cloudsearch expects all JSON objects to have these fields, and
 // 2) converts any value fields that are of type "object" to type string, since Cloudsearch cannot index json objects as field values.
 var uuid = require('node-uuid');
+var fs = require('fs');
 var root = '/Users/srice/Desktop/';
 var documentsFile = root + 'allHealthPlans.json';
 var destination = root + 'csAllHealthPlans.json';
 
 try {
-  var csAllHealthPlans = [];
-  with open(documentsFile) as
-  jsonFile:
-    allHealthPlans = json.load(jsonFile)
-
-  for(var healthPlan in allHealthPlans) {
-    for(var key, value in healthPlan.iteritems()) {
-      if(typeof value === "Object") {
-        for(var key2, value2 in value.iteritems()) {
-          healthPlan[key] = value2;
+  fs.readFile(documentsFile, function(err, data) {
+    var csAllHealthPlans = [];
+    if(err) {
+      console.log(err);
+    } else {
+      var allHealthPlans = JSON.parse(data);
+      for(var i = 0; i < allHealthPlans.length; i++) {
+        var healthPlan = allHealthPlans[i];
+        for(var key in healthPlan) {
+          if(healthPlan.hasOwnProperty(key) && typeof healthPlan[key] === "Object") {
+            for(var key2 in healthPlan[key]) {
+              if(healthPlan[key].hasOwnProperty(key2)) {
+                healthPlan[key] = healthPlan[key][key2];
+              }
+            }
+          }
         }
+
+        var csHealhPlan = {
+          "type": "add",
+          "id": uuid.v4()
+        };
+
+        csHealhPlan["fields"] = healthPlan;
+        csAllHealthPlans.push(csHealhPlan);
       }
     }
 
-    var csHealhPlan = {
-      "type": "add",
-      "id": uuid.v4()
-    };
+    writeFile(csAllHealthPlans);
+  });
 
-    csHealhPlan["fields"] = healthPlan;
-    csAllHealthPlans.push(csHealhPlan);
+  var writeFile = function(csAllHealthPlans) {
+    fs.writeFile(destination, JSON.stringify(csAllHealthPlans, null, 4), function(err) {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log("JSON saved to " + destination);
+      }
+    });
   }
-
-  with open(destination, 'w') as
-  outfile:
-    json.dump(csAllHealthPlans, outfile)
-  print
-  "Writing file to file at /Users/srice/Desktop/csAllHealthPlans.json"
-} catch(IOError
-as(errno, strerror)
-)
-{
-  print
-  "I/O error({0}): {1}".format(errno, strerror)
+} catch(err) {
+  console.log("Error" + err);
 }
